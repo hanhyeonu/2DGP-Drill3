fe55f7c8b04ba1366dcb69fb4dafb78b9b1b4d16
Hanu(LAPTOP) <hhw3078@gmail.com>
2025-09-17 22:18:44 +0900

    move_rectangle 함수에 이동로직 추가

--- character_moves.py
+++ character_moves.py
@@ -1,13 +1,32 @@
 from pico2d import *
-import math
 
 open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+x, y = 400, 90
+
 def move_rectangle():
-    pass
+    global x, y
+    while True:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+
+        if y == 90 and x < 784:
+            x = x + 2
+        elif x >= 784 and y < 560:
+            y = y + 2
+        elif y >= 560 and x > 16:
+            x = x - 2
+        elif x <= 16 and y > 90:
+            y = y - 2
+
+        delay(0.01)
+
+        if x == 400 and y == 90:
+            break
 
 
 def move_circle():
