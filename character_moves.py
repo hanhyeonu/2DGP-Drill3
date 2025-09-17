0d4401041fd54ed43de37882a4d7aba8fa725652
Hanu(LAPTOP) <hhw3078@gmail.com>
2025-09-17 19:58:40 +0900

    Drill2 의 move 함수를 move_rectangle 과 move_circle로 분리 - 이유 : 삼각형 운동을 추가할때 기존의 MoveType을 사용하기 까다롭다고 생각함

---
+++ character_moves.py
@@ -0,0 +1,19 @@
+from pico2d import *
+import math
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+def move_rectangle():
+    pass
+
+
+def move_circle():
+   pass
+
+
+while True:
+    move_rectangle()
+    move_circle()
